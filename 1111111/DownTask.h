b7f3fdc3156099a94ff1e6dd5bc2fbb0e1df8d41
Henry Lei <244482972@qq.com>
2016-03-16 09:42:13 +0800

    一些简单的存储方式

---
+++ 1111111/DownTask.h
@@ -0,0 +1,13 @@
+//
+//  DownTask.h
+//  1111111
+//
+//  Created by App Team on 16/1/27.
+//  Copyright © 2016年 test. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DownTask : NSObject
+
+@end
